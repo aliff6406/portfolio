1134bbf780d98a6fb3a7e7a83cd36419f93a7b30
Aliff Amir <aliff6406@gmail.com>
2024-09-26 00:39:51 +0100

    chore: remove commented code

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -12,14 +12,8 @@ export const Experience = () => {
       <h2 className="mb-7 font-bold tracking-tighter sm:text-3xl md:text-4xl">
         Work Experience
       </h2>{" "}
-      {/* </BlurFade> */}
-      {/* <BlurFade delay={BLUR_FADE_DELAY * 14}> */}
       <ul className="mb-4 ml-4 divide-y divide-dashed border-l">
         {experiences.map((experience, idx) => (
-          // <BlurFade
-          //   key={experience.title + project.dates}
-          //   delay={BLUR_FADE_DELAY * 15 + id * 0.05}
-          // >
           <ExperienceCard
             key={idx}
             title={experience.title}
@@ -30,10 +24,8 @@ export const Experience = () => {
             image={experience.src}
             //   links={experience.links}
           />
-          // </BlurFade>
         ))}
       </ul>
-      {/* </BlurFade> */}
     </section>
   );
 };
