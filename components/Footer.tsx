7a289707b4b66cb65bd7b40037cc9fed8e39d0ec
Aliff Amir <aliff6406@gmail.com>
2024-09-28 23:41:11 +0100

    feat: fix photo collage responsiveness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,14 +7,24 @@ const Footer = () => {
       className="flex h-[13rem] w-full flex-col items-center justify-center gap-10 bg-gray-100"
     >
       <div className="flex items-center justify-center gap-8">
-        
-        <Image src="/github.svg" alt="github" width={42} height={42}></Image>
-        <Image
-          src="/linkedin.svg"
-          alt="linkedin"
-          width={42}
-          height={42}
-        ></Image>
+        <a href="https://github.com/aliff6406">
+          <Image
+            className="duration-250 transition ease-in-out hover:-translate-y-2"
+            src="/github.svg"
+            alt="github"
+            width={42}
+            height={42}
+          ></Image>
+        </a>
+        <a href="https://www.linkedin.com/in/aliffamir/">
+          <Image
+            className="duration-250 transition ease-in-out hover:-translate-y-2"
+            src="/linkedin.svg"
+            alt="linkedin"
+            width={42}
+            height={42}
+          ></Image>
+        </a>
       </div>
       <p className="text-sm text-gray-500">
         Copyright &copy; 2024 Aliff Amir. All rights reserved.
--- components/PhotoCollage.tsx
+++ components/PhotoCollage.tsx
@@ -3,20 +3,31 @@ import Image from "next/image";
 
 const PhotoCollage = () => {
   return (
-    <div className="relative mb-20 flex items-center justify-center">
+    <div className="relative mb-20 flex flex-col items-center justify-center lg:flex-row lg:space-x-4">
       {/* Left Image */}
-      <div className="absolute left-[30%] z-10 -translate-x-full transform overflow-hidden rounded-2xl object-cover shadow-2xl">
-        <Image src="/timmy.jpg" alt="Left Image" width={300} height={300} />
+      <div className="absolute left-[30%] z-10 w-[30%] -translate-x-full transform overflow-hidden rounded-2xl object-cover shadow-2xl sm:w-[30%] md:w-[30%] lg:w-[40%]">
+        <Image src="/timmy.jpg" alt="Left Image" width={1000} height={1000} />
       </div>
 
       {/* Center Image */}
-      <div className="relative z-20 overflow-hidden rounded-2xl object-cover shadow-2xl">
-        <Image src="/profile.jpg" alt="Center Image" width={350} height={500} />
+      <div className="relative z-20 w-[50%] overflow-hidden rounded-2xl object-cover shadow-2xl sm:w-[45%] md:w-[40%] lg:w-[50%]">
+        <Image
+          src="/profile.jpg"
+          className="object-cover"
+          alt="Center Image"
+          width={1000}
+          height={1000}
+        />
       </div>
 
       {/* Right Image */}
-      <div className="absolute right-[35%] top-[15%] z-10 translate-x-full transform overflow-hidden rounded-2xl object-cover shadow-2xl">
-        <Image src="/pastries.jpg" alt="Right Image" width={400} height={300} />
+      <div className="absolute right-[35%] top-[15%] z-10 w-[35%] translate-x-full transform overflow-hidden rounded-2xl object-cover shadow-2xl sm:w-[40%] md:w-[40%] lg:w-[50%]">
+        <Image
+          src="/pastries.jpg"
+          alt="Right Image"
+          width={1000}
+          height={1000}
+        />
       </div>
     </div>
   );
